14e222dca17755e1d930effff27b36d323da3019
Aiden Luce <luceaiden4124@gmail.com>
2023-06-02 16:11:29 -0500

    use state gotche

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,18 @@
+import {useState} from "react";
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+    const [value , setValue] = useState(0)
+
+    const handleClick = ()=>{
+        setValue((currentState)=>{
+            return currentState +1
+        })
+    }
+  return (
+          <>
+            <h1>{value}</h1>
+            <button type='button' className='btn' onClick={handleClick}>Increase</button>
+          </>
+        )
 };
 
 export default UseStateGotcha;
