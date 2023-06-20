03dbdd60c3945b630755bd4c1a7f1265b99b9ca8
Aiden Luce <luceaiden4124@gmail.com>
2023-06-20 13:26:12 -0500

    multiple returns

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
   const [user, setUser] = useState(null)
 
   useEffect(()=>{
@@ -9,14 +11,28 @@ const MultipleReturnsFetchData = () => {
       try{
         const resp = await fetch(url)
         const user = resp.json()
+        setUser(user)
         console.log(user)
       } catch (error){
-        console.log(error)
+        setIsError(true)
       }
+      setIsLoading(false)
     }
     fetchUser()
   },[])
 
-  return <h2>Fetch Data </h2>;
+  if(isLoading){
+    return <h2>Loading...</h2>
+  }
+  if(isError){
+    return <h2>There was an error...</h2>
+  }
+  return (
+      <div>
+        <h2>{user.name}</h2>
+        <h4>Works at {user.company}</h4>
+        <p>{user.bio}</p>
+      </div>
+  );
 };
 export default MultipleReturnsFetchData;
