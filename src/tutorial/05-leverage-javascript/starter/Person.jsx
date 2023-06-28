66c66d6a98e01926ed4a02ccd4a03d7130a65e5d
Aiden Luce <luceaiden4124@gmail.com>
2023-06-28 13:58:43 -0500

    Leveraging Javascript Complete

--- src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-export function Person({name, nickName, images}){
 
-    const img = images[0].small.url
+import avatar from "../../../assets/default-avatar.svg"
+
+export function Person({name, nickName ='N/A', images}){
+
+    const img = images?.[0]?.small?.url || avatar
 
     return <div>
-        <img src={img} alt={name}/>
+        <img src={img} alt={name} style={{width: "50px"}}/>
         <h4>{name}</h4>
         <p>Nickname : {nickName}</p>
     </div>
